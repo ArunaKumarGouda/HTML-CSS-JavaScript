96b0769a4f72e75c75c615dc003e94c16ecbdad5
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-02 10:22:12 +0530

    Add variable declarations and console logs

---
+++ JavaScript/02_variable.js
@@ -0,0 +1,16 @@
+age = 19;
+Name = "Aruna Kumar Gouda";
+
+x = null;
+y = undefined;
+
+console.log(age);
+console.log(Name);
+console.log(x);
+console.log(y);
+
+isFollow = false;
+positive = true;
+
+console.log(isFollow);  
+console.log(positive);
