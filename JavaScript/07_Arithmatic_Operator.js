555cb06559f22cb61366a447dd89ef85f2bd0252
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-10 00:44:31 +0530

    Create 07_Arithmatic_Operator.js

---
+++ JavaScript/07_Arithmatic_Operator.js
@@ -0,0 +1,29 @@
+let a = 5;
+let b = 2;
+
+console.log("a =", a, "&", "b =", b);
+
+
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a ** b =", a ** b);
+
+// Unary Operator
+{
+    let a = 5;
+    let b = 4;
+
+    console.log("a =", a, "&", "b =", b);
+
+    console.log("a++ =", a++);
+    console.log("a =", a);
+    console.log("++a =", ++a);
+    console.log("a =", a);
+    console.log("b-- =", b--);
+    console.log("b =", b);
+    console.log("--b =", --b);
+    console.log("b =", b);
+}
