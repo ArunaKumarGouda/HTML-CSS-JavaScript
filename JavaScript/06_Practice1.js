d7cbb3b7bc235f885330892bd8a43a36d9d8d92e
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-09 17:51:21 +0530

    Create 06_Practice1.js

---
+++ JavaScript/06_Practice1.js
@@ -0,0 +1,16 @@
+const product = {
+    name: "Parker Jotter Standard CT Ball Pen",
+    color: "Black",
+    rating: 4.1,
+    price: 270,
+    offer: 5,
+    isAvailable: true,
+};
+
+console.log(product);   
+
+console.log(typeof product);
+console.log(typeof product.name);
+console.log(typeof product.rating);
+console.log(typeof product.isAvailable);
+console.log(typeof product.offer);
