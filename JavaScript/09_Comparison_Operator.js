d6fcd4d251dc792f9e603b995f4b0c294826145f
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-11 18:30:09 +0530

    Add comparison operator examples in JavaScript

---
+++ JavaScript/09_Comparison_Operator.js
@@ -0,0 +1,37 @@
+{
+    let a = 5;
+    let b = 5;
+
+    console.log("a =", a, " & ", "b = ", b);
+    console.log("a == b :", a == b);
+    console.log("a != b :", a != b);
+    console.log("a === b :", a === b);  // a === b: check comparison and datatype.
+}
+{
+    let a = 5;
+    let b = 4;
+
+    console.log("a =", a, " & ", "b = ", b);
+    console.log("a == b :", a == b);
+    console.log("a != b :", a != b);
+}
+{
+    let a = 4;
+    let b = "4";
+    
+    console.log("a =", a, " & ", "b = ", b);
+    console.log("a == b :", a == b);
+    console.log("a === b :", a === b);
+    console.log("a !== b :", a !== b);
+}
+
+{
+    let a = 5;
+    let b = 4;
+
+    console.log("a =", a, " & ", "b = ", b);
+    console.log("a > b :", a > b);
+    console.log("a >= b :", a >= b);
+    console.log("a < b :", a < b);
+    console.log("a <= b :", a <= b);
+}
