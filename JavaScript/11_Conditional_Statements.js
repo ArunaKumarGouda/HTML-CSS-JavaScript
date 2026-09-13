b93ca1dcb4dcf9586eb06ef7be2d6d4dfc7ff51e
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-13 13:00:31 +0530

    Add conditional statements for voting and color mode

---
+++ JavaScript/11_Conditional_Statements.js
@@ -0,0 +1,25 @@
+// Conditional statements
+
+{
+    let age = 16;
+
+    if(age >= 18) {
+        console.log("You can vote.");
+    }
+
+    if(age < 18) {
+        console.log("You con not vote")
+    }
+};
+{
+    let mode = "dark";
+    let color;
+
+    if(mode == "dark") {
+        color = "Black";
+    } else {
+        color = "White";
+    }
+
+    console.log(color);
+ };
