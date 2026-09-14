097eefedcb69987bbe8753fbc16ee6507df4585c
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-14 09:14:51 +0530

    Add else if statement examples in 13_else_if_statement.js

---
+++ JavaScript/13_else_if_statement.js
@@ -0,0 +1,28 @@
+{
+    let age = 21;
+
+    if(age < 18) {
+        console.log("Junior");
+    } else if(age > 60) {
+        console.log("Senior");
+    } else {
+        console.log("Middle");
+    }
+};
+
+{
+    let mode = "blue";
+    let color;
+
+    if(mode === "dark") {
+        color = "dark";
+    } else if(mode === "blue") {
+        color = "blue";
+    } else if(mode === "pink") {
+        color = "pink";
+    } else {
+        color = "white";
+    }
+
+    console.log(color);
+};
