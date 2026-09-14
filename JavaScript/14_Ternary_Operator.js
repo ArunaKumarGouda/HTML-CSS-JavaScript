bfe43764636ced76ac6b16edd7e6eba2a30f8987
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-14 09:15:23 +0530

    Add examples of using the ternary operator

---
+++ JavaScript/14_Ternary_Operator.js
@@ -0,0 +1,12 @@
+{
+    let age = 21;
+
+    let result = age >= 18 ? "adult" : "not adult";
+    console.log(result);
+};
+
+{
+    let age = 16;
+
+    age >= 18 ? console.log("adult") : console.log("not adult");
+};
