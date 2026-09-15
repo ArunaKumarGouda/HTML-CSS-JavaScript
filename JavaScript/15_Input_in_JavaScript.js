1f7016914c26181f4bdf9d4897bcc338e3ef12b7
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-15 23:20:58 +0530

    Create 15_Input_in_JavaScript.js

---
+++ JavaScript/15_Input_in_JavaScript.js
@@ -0,0 +1,13 @@
+{
+    let name = prompt("Enter your name: ");
+    console.log("Hello!", name);   
+};
+
+{
+    let num = prompt("Enter number: ");
+    if(num % 2 == 0) {
+        console.log("Number is even");
+    } else {
+        console.log("Number is odd");
+    }
+};
