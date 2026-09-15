e33900027a5464135163530df026563cb7b03ab6
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-16 02:44:40 +0530

    Implement loops for name printing and sum calculation
    Add loops to print a name and calculate sum.

---
+++ JavaScript/18_Loops.js
@@ -0,0 +1,12 @@
+// Loops are used to execute a piece of code agian and again.
+for(let i = 1; i <= 5; i++) {
+    console.log("Aruna Kumar Gouda");
+}
+
+let sum = 0;
+let n = 5;
+for(let i = 1; i <= n; i++) {
+    sum += i;
+}
+console.log("sum =", sum);
+console.log("End of loop");
