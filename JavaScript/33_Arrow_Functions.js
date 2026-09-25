6a1ed203f77335e85435cf848b07a88fb0778e4d
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-25 10:58:59 +0530

    Implement arrow functions for arithmetic and greeting
    Added an arrow function to perform basic arithmetic operations and a function to print 'Hello'.

---
+++ JavaScript/33_Arrow_Functions.js
@@ -0,0 +1,13 @@
+// Arrow Function: Compact way to write a function
+
+const sum = (a, b) => {
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+    console.log(a / b);
+}
+
+sum(4, 5);
+
+const printHello = () => console.log("Hello");
+printHello();
