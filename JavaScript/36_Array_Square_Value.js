48070a0473d4e30da810ec1b74d776da16c01ace
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-26 19:07:24 +0530

    Add function to print square of array values

---
+++ JavaScript/36_Array_Square_Value.js
@@ -0,0 +1,7 @@
+// Q: For a given array of numbers, print the square of each value using the forEach function.
+
+let arr = [2, 5, 3, 1, 4];
+
+arr.forEach(function square(val) {
+    console.log(val ** 2);
+})
